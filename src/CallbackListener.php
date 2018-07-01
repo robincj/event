3b807e8732c30f347d485ca2e5734952009fb6e7
robincj <dev@cameron-jones.net>
2018-07-01 21:14:47 +1200

    Added facility to retrieve response data from Listener callback

--- src/CallbackListener.php
+++ src/CallbackListener.php
@@ -1,16 +1,30 @@
 <?php
-
 namespace League\Event;
 
 class CallbackListener implements ListenerInterface
 {
+    
     /**
      * The callback.
      *
      * @var callable
      */
     protected $callback;
-
+    
+    /**
+     * The last response from the callback
+     *
+     * @var mixed
+     */
+    protected $response;
+    
+    /**
+     * The last ListenerResponse from the callback
+     *
+     * @var ListenerResponse
+     */
+    protected $listenerResponse;
+    
     /**
      * Create a new callback listener instance.
      *
@@ -20,7 +34,7 @@ class CallbackListener implements ListenerInterface
     {
         $this->callback = $callback;
     }
-
+    
     /**
      * Get the callback.
      *
@@ -30,16 +44,50 @@ class CallbackListener implements ListenerInterface
     {
         return $this->callback;
     }
-
+    
     /**
+     *
      * @inheritdoc
      */
     public function handle(EventInterface $event)
     {
-        call_user_func_array($this->callback, func_get_args());
+        $this->listenerResponse = NULL;
+        $this->response = call_user_func_array($this->callback, func_get_args());
+        return $this;
     }
-
+    
     /**
+     * Get the raw response from the last callback
+     *
+     * @return mixed
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+    
+    /**
+     * Get the ListenerResponse object from the last callback.
+     * If the callback did not return a ListenerResponse then the response is converted into one.
+     *
+     * @return \League\Event\ListenerResponse|mixed
+     */
+    public function getListenerResponse()
+    {
+        if (! $this->listenerResponse) {
+            $listenerResponse = $this->response;
+            if (! is_a($this->response, 'ListenerResponseInterface')) {
+                // Treat the response as data
+                $listenerResponse = new ListenerResponse();
+                $listenerResponse->setEvent($event)->setData($response);
+            }
+            $this->listenerResponse = $listenerResponse;
+        }
+        return $this->listenerResponse;
+    }
+    
+    /**
+     *
      * @inheritdoc
      */
     public function isListener($listener)
@@ -47,10 +95,10 @@ class CallbackListener implements ListenerInterface
         if ($listener instanceof CallbackListener) {
             $listener = $listener->getCallback();
         }
-
+        
         return $this->callback === $listener;
     }
-
+    
     /**
      * Named constructor
      *
